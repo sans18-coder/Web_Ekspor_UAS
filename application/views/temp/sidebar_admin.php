757f71d184e2afb5763963bf2f531c6287e42092
nesa <alamat-email-anda@example.com>
2023-12-04 14:34:28 +0700

    menambahkan sidebar admin, membuat kerangka admin index dan admin accepted

---
+++ application/views/temp/sidebar_admin.php
@@ -0,0 +1,62 @@
+<!-- Sidebar -->
+<ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style=" background: linear-gradient(225deg, rgba(116, 14, 15, 1) 15%,rgba(0, 0, 0, 1) 100%);">
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
+        <div class="sidebar-brand-icon">
+            <i class="fas fa-user me-2" style="font-size: 20px;"></i>
+        </div>
+        <div class="sidebar-brand-text ms-1">Admin</div>
+    </a>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+    <!-- Looping Menu-->
+    <div class="sidebar-heading">Submission</div>
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link pb-0" href="<?= base_url(''); ?>"><!--ini ntar ubah ke buku/kategori -->
+            <i class="fa fa-table"></i>
+            <span>Submission</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link pb-0" href="<?= base_url(''); ?>"><!--ini ntar ubah ke buku/kategori -->
+            <i class="fas fa-history"></i>
+            <span>Submission Accepted</span>
+        </a>
+    </li>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider mt-3">
+    <!-- Heading -->
+    <div class="sidebar-heading">Produk</div>
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link pb-0" href="<?= base_url(''); ?>"><!--ini ntar ubah ke buku/kategori -->
+            <i class="fas fa-plus-square"></i>
+            <span>Tambah Produk</span></a>
+    </li>
+   
+     <li class="nav-item">
+        <a class="nav-link pb-0" href="<?= base_url(''); ?>">
+            <i class="fas fa-trash"></i>
+            <span>Hapus Produk</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link pb-0" href="<?= base_url(''); ?>">
+            <i class="fas fa-edit"></i>
+            <span>Update Produk</span></a>
+    </li>
+    </li>
+    <!-- Divider -->
+   
+    <!-- Divider -->
+    <hr class="sidebar-divider mt-3">
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
+<!-- End of Sidebar -->
