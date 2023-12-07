0f100761312eb7b5c46b1be0ffa30ef9ffccaf3a
potatocip <145647632+potatocip@users.noreply.github.com>
2023-12-07 14:36:24 +0700

    Update footer.php

--- application/views/temp/footer.php
+++ application/views/temp/footer.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto ">
-            <span class="text-dark">Local Coffee &copy; Web Programming II Kelompok IV <?= date('Y'); ?></span>
+            <span class="text-dark">Lokal Coffee &copy; Web Programming II Kelompok IV <?= date('Y'); ?></span>
         </div>
     </div>
 </footer>
