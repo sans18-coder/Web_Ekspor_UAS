765e8a9dced7c5f3becd91359a3ba4bb2b00438a
hindami23 <145647671+hindami23@users.noreply.github.com>
2023-12-10 21:42:52 +0700

    Update header_login_admin.php HINDAMI

--- application/views/temp/header_login_admin.php
+++ application/views/temp/header_login_admin.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>asset/bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>asset/css/loginstyle.css">
 </head>
 
 <body class="m-0 p-0">
@@ -14,7 +15,7 @@
         <!-- header -->
         <header>
             <!-- navbar -->
-            <nav class="navbar navbar-expand-lg border-bottom shadow-sm bg-transparent pt-1 pb-1">
+            <nav class="navbar navbar-expand-lg shadow-sm position-fixed col-12 pt-1 pb-1">
                 <div class="container-fluid">
                     <h1 class="navbar-brand fw-bold fs-3" href="#">Lokal coffee</h1>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,7 +24,7 @@
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav nav-underline ms-auto pe-4 fw-bold">
                             <li class="nav-item ps-3">
-                                <a class="nav-link" href="<?php echo base_url() . 'index.php/admin'; ?>">Admin</a>
+                                <a class="nav-link" href="<?php echo base_url() . 'index.php/admin'; ?>">ADMIN</a>
                             </li>
                         </ul>
                     </div>
