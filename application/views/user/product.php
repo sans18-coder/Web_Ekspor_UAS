c7df2b276b8ed9a9e01d53ad06cee945b6b1070d
Jappler <145647589+Jappler@users.noreply.github.com>
2023-12-11 13:57:34 +0700

    Update product.php

--- application/views/user/product.php
+++ application/views/user/product.php
@@ -1,100 +1,28 @@
-<?php
-$base_url = base_url();
-$d = $base_url . 'asset/image/product/';
-?>
-<main class="container-fluid">
-    <section class="product-display text-center col-12">
-        <img src="<?= $d; ?>kopi.jpeg" alt="Product Image" class="img-fluid">
-        <hr class="divider my-5" style="width: 100%;">
-        <div class="product-description">
-            <h2 class="mb-3">Kopi Toraja Sulawesi</h2>
-            <div class="row">
-                <div class="col-md-6 order-md-first">
-                    <h2 class="name">Kopi Toraja Sulawesi</h2>
-                    <p>Kopi Toraja Sulawesi memiliki aroma kopi spesial yang khas dengan tingkat keasaman yang rendah, halus, lembut, citarasa floral dan fruity. Sensasi rasa kopinya kuat dengan ciri karakter rempah-rempah atau kacang-kacangan seperti kayu manis atau cardamon (sejenis jahe)</p>
-                </div>
-                <div class="col-6">
-                    <img src="<?= $d; ?>Kopi_Toraja.jpg" alt="Product Image">
-                </div>
-            </div>
-            <hr>
-            <h2 class="mb-3">Kopi Arabica Gayo</h2>
-            <div class="row">
-                <div class="col-md-6 order-md-first">
-                    <h2 class="name">Kopi Arabica Gayo</h2>
-                    <p>Kopi Arabica Gayo memiliki citarasa nutty dan buttery. Aromanya sangat kuat dengan aksen nutty dan spice. Keasamannya sangat rendah, sweetness cenderung tinggi, dan body yang dihasilkan medium.</p>
-                </div>
-                <div class="col-6">
-                    <img src="<?= $d; ?>Kopi_Arabica_Gayo.jpg" alt="Product Image">
-                </div>
-            </div>
-            <hr>
-            <h2 class="mb-3">Kopi Kintamani Bali</h2>
-            <div class="row">
-                <div class="col-md-6 order-md-first">
-                    <h2 class="name">Kopi Kintamani Bali</h2>
-                    <p>Kopi Kintamani Bali memiliki cita rasa asam segar seperti buah jeruk (citrusy) tanpa meninggalkan aftertaste di mulut. Body-nya medium dan aroma yang dihasilkan sangat kuat dan manis dengan sentuhan bunga dan rempah rempah.</p>
-                </div>
-                <div class="col-6">
-                    <img src="<?= $d; ?>Kopi_Kintamani_Bali.jpg" alt="Product Image" style="min-width: 300px;">
-                </div>
-            </div>
-            <hr>
-            <h2 class="mb-3">Kopi Robusta Lampung</h2>
-            <div class="row">
-                <div class="col-md-6 order-md-first">
-                    <h2 class="name">Kopi Robusta Lampung</h2>
-                    <p>kopi Robusta Lampung cenderung lebih pahit dan kurang asam dengan memiliki tubuh yang lebih berat dan tekstur yang lebih kental serta aroma yang khas yaitu sperti bau tanah, kayu, dan rempah-rempah.</p>
-                </div>
-                <div class="col-6">
-                    <img src="<?= $d; ?>Kopi_Lampung.jpg" alt="Product Image" style="max-width: 300px;">
-                </div>
-            </div>
-            <hr>
-            <h2 class="mb-3">Kopi Papua Wamena</h2>
-            <div class="row">
-                <div class="col-md-6 order-md-first">
-                    <h2 class="name">Kopi Papua Wamena</h2>
-                    <p>Kopi Papua Wamena memiliki aroma yang khas dengan sentuhan buah tropis, bunga, dan rempah-rempah. Rasanya yang bervariasi dengan rasa buah, cokelat, dan rempah-rempah dengan asam yang lembut.</p>
-                </div>
-                <div class="col-6">
-                    <img src="<?= $d; ?>Kopi_Wamena_Papua.jpg" alt="Product Image">
-                </div>
-            </div>
-            <hr>
-            <h2 class="mb-3">Kopi Mandailing</h2>
-            <div class="row">
-                <div class="col-md-6 order-md-first">
-                    <h2 class="name">Kopi Mandailing</h2>
-                    <p>Kopi Mandailing memiliki aroma yang khas dengan sentuhan rempah-rempah, kayu, dan cokelat. Rasanya bisa mencakup rasa cokelat, bunga, rempah-rempah, dan kadang-kadang dengan sentuhan buah. Rasanya kompleks dan memuaskan.</p>
-                </div>
-                <div class="col-6">
-                    <img src="<?= $d; ?>Kopi_Mandailing.jpg" alt="Product Image">
-                </div>
-            </div>
-            <hr>
-            <h2 class="mb-3">Kopi Flores Bajawa</h2>
-            <div class="row">
-                <div class="col-md-6 order-md-first">
-                    <h2 class="name">Kopi Flores Bajawa</h2>
-                    <p>Kopi Flores Bajawa memiliki aroma yang khas dengan sentuhan buah tropis, bunga, dan rempah-rempah. Rasa kopinya memiliki asam yang hidup dan cerah, dengan sentuhan cokelat, rempah-rempah, dan buah.</p>
-                </div>
-                <div class="col-6">
-                    <img src="<?= $d; ?>Kopi_Flores.jpg" alt="Product Image">
-                </div>
-            </div>
-            <hr>
-            <h2 class="mb-3">Kopi Java</h2>
-            <div class="row">
-                <div class="col-md-6 order-md-first">
-                    <h2 class="name">Kopi Java</h2>
-                    <p>Kopi Java memiliki aroma yang khas yaitu buah, bunga, kayu, dan rempah-rempah. Rasa kopi ini bervariasi, tetapi seringkali menawarkan rasa yang halus, seimbang, dan konsisten. Rasanya bisa mencakup cokelat, buah-buahan, dan kadang-kadang dengan sentuhan rempah-rempah.</p>
-                </div>
-                <div class="col-6">
-                    <img src="<?= $d; ?>Kopi_Java.jpg" alt="Product Image">
-                </div>
-            </div>
-            <hr>
+<main class="container-fluid mx-auto">
+    <?php
+    $d = base_url() . 'asset/image/product/';
+    ?>
+    <section class="product-display row text-center">
+        <div class="col-10 mx-auto">
+            <img src=" <?= $d . 'kopi.jpeg' ?>" alt="Product Image" class="img-fluid">
         </div>
+        <?php foreach ($produk as $p) {
+        ?>
+            <hr class="divider my-5" style="width: 100%;">
+            <div class="col-12 d-flex justify-content-center">
+                <div class="row col-9">
+                    <div class="col-md-8 margin left-5 mx-auto">
+                        <div class="product-description">
+                            <h2 class style="text-align: center" ;="judul"><?= $p['productName'] ?></h2>
+                            <p style="text-align:left;"><?= $p['productDes'] ?></p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="<?= $d . $p['productImage']; ?>" alt="Product Image" class="align-self-center mr-3 img-fluid" style="width: 300px">
+                    </div>
+                </div>
+                <hr>
+            </div>
+        <?php } ?>
     </section>
 </main>
